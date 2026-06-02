38561244527efa6a3f31cf7c817f09f5d0f91d09
Astrais Corporations <astraiscorporations@icloud.com>
2026-06-02 20:38:35 +0700

    11. Patch run.main.py

--- main/Libraries/libcontent/run.main.py
+++ main/Libraries/libcontent/run.main.py
@@ -1,8 +1,8 @@
 import subprocess
 
 def run(arg):
-    command = arg.split(sep=" ")
-    result = subprocess.run(command)
+	command = arg.split(sep=" ")
+	result = subprocess.run(command)
 	return result.returncode
 
 register_function("run", run)
